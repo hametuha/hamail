eff25b311416490ef252868f8dcbeded96dd90d4
fumikito <guy@hametuha.com>
2018-06-18 01:53:06 +0900

    Remove parentheses for Sendgrid sanitization.

---
+++ hooks/user-registeration.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User notification for registration.
+ *
+ * @package hamail
+ */
+
+/**
+ * Filter registration email.
+ *
+ * @param array   $wp_new_user_notification_email
+ * @param WP_User $user
+ * @param string  $blogname
+ *
+ * @return array
+ */
+add_filter( 'wp_new_user_notification_email', function( $wp_new_user_notification_email, $user, $blogname ){
+	$wp_new_user_notification_email['message'] = preg_replace( '#<(https?://[^>]+)>#u', '$1', $wp_new_user_notification_email['message'] );
+	return $wp_new_user_notification_email;
+}, 10, 3 );
+
+/**
+ * Filter reset password mail
+ *
+ * @param string $message
+ * @return string
+ */
+add_filter( 'retrieve_password_message', function( $message ) {
+	$message = preg_replace( '#<(https?://[^>]+)>#u', '$1', $message );
+	return $message;
+} );
