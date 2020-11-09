8f1a27ff8afa8569ef982adbef8e402d835c73f1
fumikito <guy@hametuha.com>
2020-11-09 19:49:07 +0900

    #25 Fix sed option according to https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux

--- bin/build.sh
+++ bin/build.sh
@@ -27,14 +27,14 @@ npm run package
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
 
 # Change version string.
-sed -i "s/^Version: .*/Version: ${VERSION}/g" ./hamail.php
-sed -i "s/^Stable Tag: .*: .*/Stable Tag: ${VERSION}/g" ./readme.txt
+sed -i.bak "s/^Version: .*/Version: ${VERSION}/g" ./hamail.php
+sed -i.bak "s/^Stable Tag: .*: .*/Stable Tag: ${VERSION}/g" ./readme.txt
 
 
 if [ "pro" = $2 ]; then
   # If pro, change name.
-  sed -i "s/^Plugin Name: .*/Plugin Name: Hamail PRO/g" ./hamail.php
-  sed -i "s/^Plugin URI: .*/Plugin URI: https:\/\/kunoichiwp.com\/product\/plugin\/hamail-pro/g" ./hamail.php
+  sed -i.bak "s/^Plugin Name: .*/Plugin Name: Hamail PRO/g" ./hamail.php
+  sed -i.bak "s/^Plugin URI: .*/Plugin URI: https:\/\/kunoichiwp.com\/product\/plugin\/hamail-pro/g" ./hamail.php
 else
   # IF light version, remove pro dir.
   rm -rf ./app/Hametuha/Hamail/Pro
@@ -50,8 +50,11 @@ rm -rf .eslintrc
 rm -rf .phpcs.xml
 rm -rf bin
 rm -rf node_modules
+rm -rf README.md
 rm -rf tests
 rm -rf phpunit.xml.dist
 rm -rf stylelint.config.js
 rm -rf webpack.config.js
 rm -rf .wp-env.json
+rm -rf hamail.php.bak
+rm -rf readme.txt.bak
