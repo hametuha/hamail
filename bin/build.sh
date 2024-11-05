3cfc414d0c21094c12fc94cc5c6e9e8483eb5a06
fumikito <guy@hametuha.com>
2024-11-05 18:17:16 +0900

    Fix build files.

--- bin/build.sh
+++ bin/build.sh
@@ -11,9 +11,7 @@ fi
 PREFIX="refs/tags/"
 VERSION=${1#"$PREFIX"}
 
-WP_README_ENV=$2
-
-echo "Building Hamail ${WP_README_ENV} v${VERSION}..."
+echo "Building Hamail v${VERSION}..."
 
 
 # Install packages.
@@ -30,13 +28,3 @@ curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.ph
 # Change version string.
 sed -i.bak "s/^Version: .*/Version: ${VERSION}/g" ./hamail.php
 sed -i.bak "s/^Stable Tag: .*/Stable Tag: ${VERSION}/g" ./readme.txt
-
-# Tweaks for pro/light
-if [ "pro" = $2 ]; then
-  # If pro, change name.
-  sed -i.bak "s/^Plugin Name: .*/Plugin Name: Hamail PRO/g" ./hamail.php
-  sed -i.bak "s/^Plugin URI: .*/Plugin URI: https:\/\/kunoichiwp.com\/product\/plugin\/hamail-pro/g" ./hamail.php
-else
-  # IF light version, remove pro dir.
-  rm -rf ./app/Hametuha/Hamail/Pro
-fi
